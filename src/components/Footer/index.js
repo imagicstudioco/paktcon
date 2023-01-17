777e5064a2a442c799b2eb5d810b9497a500f446
Temitope Hassan <temitope.hassan.2@gmail.com>
2023-01-17 18:48:28 +0100

    footer update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -41,28 +41,19 @@ function Footer() {
           </SocialLogo>
           
           <SocialIcons>
+          <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+              <FaLinkedin />
+            </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
               <FaFacebook />
             </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
               <FaInstagram />
             </SocialIconLink>
-            <SocialIconLink
-              href={
-                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
-              }
-              rel='noopener noreferrer'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <FaYoutube />
-            </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
               <FaTwitter />
             </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
-              <FaLinkedin />
-            </SocialIconLink>
+          
           </SocialIcons>
         </SocialMediaWrap>
       </SocialMedia>
